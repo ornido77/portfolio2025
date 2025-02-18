a63fc8c66448c3e0b556f2e3142f7ff6a37dbace
Sonny Ornido <sonnyornido0@gmail.com>
2025-02-18 22:16:53 +0800

    FEAT: About Animation

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,27 +1,42 @@
 import { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
 export const HeroOrbit = ({
   children,
   size,
   rotation,
-}: PropsWithChildren<{ size: number; rotation: number }>) => {
+  spinDuration,
+  shouldOrbit = false,
+}: PropsWithChildren<{
+  size: number;
+  rotation: number;
+  spinDuration?: string;
+  shouldOrbit?: boolean;
+}>) => {
   return (
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ">
       <div
-        className="flex items-start justify-start"
-        style={{
-          transform: `rotate(${rotation}deg)`,
-          width: `${size}px`,
-          height: `${size}px`,
-        }}
+        className={twMerge(shouldOrbit === true && "animate-spin")}
+        style={{ animationDuration: spinDuration }}
       >
         <div
-          className="inline-flex "
+          className="flex items-start justify-start"
           style={{
-            transform: `rotate(${rotation * -1}deg)`,
+            transform: `rotate(${rotation}deg)`,
+            width: `${size}px`,
+            height: `${size}px`,
           }}
         >
-          {children}
+          <div className="animate-spin [animation-duration:10s]">
+            <div
+              className="inline-flex "
+              style={{
+                transform: `rotate(${rotation * -1}deg)`,
+              }}
+            >
+              {children}
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,6 +1,8 @@
 import memojiImage from "@/assets/images/memoji-computer.png";
 import ArrowDown from "@/assets/icons/arrow-down.svg";
-import grainImage from "@/assets/images/grain.jpg";
+import earthImage from "@/assets/images/earth.png";
+import marsImage from "@/assets/images/mars.png";
+import jupiterImage from "@/assets/images/jupiter.png";
 import StarIcon from "@/assets/icons/star.svg";
 import SparkleIcon from "@/assets/icons/sparkle.svg";
 import Image from "next/image";
@@ -8,51 +10,47 @@ import { HeroOrbit } from "@/components/HeroOrbit";
 
 export const HeroSection = () => {
   return (
-    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+    <div className="py-60 md:py-80 relative z-0 overflow-x-clip">
       <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
-        <div
-          className="absolute inset-0 -z-30 opacity-5"
-          style={{ backgroundImage: `url(${grainImage.src})` }}
-        ></div>
         <div className="size-[620px] hero-ring"></div>
         <div className="size-[820px] hero-ring"></div>
         <div className="size-[1020px] hero-ring"></div>
         <div className="size-[1220px] hero-ring"></div>
-        <HeroOrbit size={800} rotation={-72}>
-          <StarIcon className="size-28 text-emerald-300" />
+        <HeroOrbit size={430} rotation={-15} shouldOrbit spinDuration="30s">
+          <SparkleIcon className="size-8 text-blue-300/200" />
         </HeroOrbit>
-        <HeroOrbit size={550} rotation={20}>
-          <StarIcon className="size-12 text-emerald-300" />
+        <HeroOrbit size={440} rotation={90} shouldOrbit spinDuration="32s">
+          <SparkleIcon className="size-5 text-blue-300/20" />
         </HeroOrbit>
-        <HeroOrbit size={590} rotation={100}>
-          <StarIcon className="size-8 text-emerald-300" />
+        <HeroOrbit size={530} rotation={180} shouldOrbit spinDuration="36s">
+          <SparkleIcon className="size-10 text-blue-300/20" />
         </HeroOrbit>
-        <HeroOrbit size={430} rotation={-15}>
-          <SparkleIcon className="size-8 text-emerald-300/20" />
+        <HeroOrbit size={550} rotation={20} shouldOrbit spinDuration="36s">
+          <StarIcon className="size-12 text-blue-300" />
         </HeroOrbit>
-        <HeroOrbit size={440} rotation={90}>
-          <SparkleIcon className="size-5 text-emerald-300/20" />
+        <HeroOrbit size={590} rotation={100} shouldOrbit spinDuration="38s">
+          <StarIcon className="size-8 text-blue-300" />
         </HeroOrbit>
-        <HeroOrbit size={530} rotation={180}>
-          <SparkleIcon className="size-10 text-emerald-300/20" />
+        <HeroOrbit size={600} rotation={88} shouldOrbit spinDuration="38s">
+          <Image src={marsImage} alt="Coder" className="size-[100px]" />
         </HeroOrbit>
-        <HeroOrbit size={720} rotation={85}>
+        <HeroOrbit size={720} rotation={85} shouldOrbit spinDuration="40s">
           <div className="size-3 rounded-full bg-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={800} rotation={-32} shouldOrbit spinDuration="42s">
+          <Image src={earthImage} alt="Coder" className="size-[100px]" />
+        </HeroOrbit>
+        <HeroOrbit size={800} rotation={-72} shouldOrbit spinDuration="42s">
+          <StarIcon className="size-28 text-white-300" />
+        </HeroOrbit>
+        <HeroOrbit size={1000} rotation={172} shouldOrbit spinDuration="48s">
+          <Image src={jupiterImage} alt="Coder" className="size-[140px]" />
         </HeroOrbit>
       </div>
       <div className="container">
-        <div className="flex flex-col items-center">
-          <Image src={memojiImage} alt="Coder" className="size-[100px]" />
-          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
-            <div className="bg-green-500 size-2.5 rounded-full"></div>
-            <div className="text-sm font-medium">
-              Available for new projects
-            </div>
-          </div>
-        </div>
         <div className="max-w-lg mx-auto">
-          <h1 className="font-serif text-3xl text-center mt-8 tracking-wide">
-            Building Exceptional User Experiences
+          <h1 className="font-serif text-5xl text-center mt-8 tracking-wide">
+            Sonny Ornido
           </h1>
           <p className="mt-4 text-center text-white/60 md:text-lg">
             I specialize in creating user-centric, responsive websites. I
